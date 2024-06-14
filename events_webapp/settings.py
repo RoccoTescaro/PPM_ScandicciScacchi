0ab4f8e06e4b944430eb649d620fb666ac334ec4
RoccoTescaro <rocco.tescaro@stud.unifi.it>
2024-06-14 14:42:00 +0200

    add Procfile

--- events_webapp/settings.py
+++ events_webapp/settings.py
@@ -99,6 +99,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -148,6 +150,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
